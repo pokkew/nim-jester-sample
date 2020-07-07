2cdf2e28c2607e0637dde4fb6c5f236bc325ac6f
pokkew <brunogabrielpk@gmail.com>
2020-07-07 17:00:11 -0400

    trying karax

--- app.nim
+++ app.nim
@@ -1,43 +1,52 @@
 import jester
 import strutils
+import httpcore
 
-const REGISTER_FORM = """<html>
-    <head>
-        <title> book club! </title>
-    </head>
-    <body>
-        <h3>Register</h3>
-        Enter your email address to signup.
-        <p>
-            <b>$1</b>
-        </p>
-        <form action="/register-form" method="POST">
-            <input type="text" name="email" />
-            <button type="submit">Send Email Address</button>
-        </form>
-    </body>
-</html>
-"""
+import karax/ [karaxdsl, vdom]
 
-# settings:
-#     port = Port(5001)
+proc registerForm(errList: seq[string]): string = 
+  let vnode = buildHtml(html):
+    head:
+      title: text "book club"
+    body:
+      h3: text "Register"
+      text "Enter your email address to sign up."
+      if errList.len > 0:
+        p:
+          text "Error(s) found."
+        ul:
+          for msg in errList:
+            li:
+              bold: text msg
+      form(action="/register-form", `method` = "POST"):
+        input(`type` = "text", name = "email")
+        button(`type` = "submit"):
+          text "Send Email Address"
+  return $vnode
+
+proc checkEmailFormat(email: string): seq[string] = 
+  result = @[]
+  var parts = email.split('@')
+  if parts.len < 2:
+    result.add "MissingAtSymbol"
 
 routes:
-    get "/":
-        resp "hello world"
-    get "/register-form":
-        resp REGISTER_FORM.format("")
-    get "/register-form/@err":
-        resp REGISTER_FORM.format(@"err")
-    post "/register-form":
-        if request.params["email"] == "":
-            redirect "/register-form/$1".format("NeedEmailAddress")
-        else:
-            redirect "/hello/$1".format(request.params["email"])
-    get "/hello/@email":
-        if @"email" == "123":
-            resp Http404
-        else:
-            resp "hello $1".format(@"email")
-    post "/":   
-        resp "something else"
+  get "/":
+    resp "hello world"
+  get "/register-form":
+    resp registerForm(@[])
+  get "/register-form/@err":
+    let errors = @"err".split("|")
+    resp registerForm(errors)
+  post "/register-form":
+    if request.params["email"] == "":
+      redirect "/register-form/$1".format("NeedEmailAddress")
+    else:
+      redirect "/hello/$1".format(request.params["email"])
+  get "/hello/@email":
+    if @"email" == "123":
+      resp Http404
+    else:
+      resp "hello $1".format(@"email")
+  post "/":   
+    resp "something else"
---
+++ hue.nim
@@ -0,0 +1,20 @@
+# const REGISTER_FORM = """<html>
+#     <head>
+#         <title> book club! </title>
+#     </head>
+#     <body>
+#         <h3>Register</h3>
+#         Enter your email address to signup.
+#         <p>
+#             <b>$1</b>
+#         </p>
+#         <form action="/register-form" method="POST">
+#             <input type="text" name="email" />
+#             <button type="submit">Send Email Address</button>
+#         </form>
+#     </body>
+# </html>
+# """
+
+# settings:
+#     port = Port(5001)
